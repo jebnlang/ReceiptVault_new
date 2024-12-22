4c701aac93528b7eac856ac7b80c5433ae427024
jebnlang <jben.lang@gmail.com>
2024-12-22 22:17:48 +0200

    Initial Commit

---
+++ ReceiptVault/ReceiptVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptVaultApp.swift
+//  ReceiptVault
+//
+//  Created by Benjamin lang on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
